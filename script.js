ceb864bcd9d0255e5da29bc62791193549e19bd1
eragon11145 <109441971+eragon11145@users.noreply.github.com>
2022-10-06 17:45:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function newgame(){
   choosegen();
   if (document.getElementById("Generations").value !="all"){
     while (gen!=parseInt(document.getElementById("Generations").value)){
-      x=Math.floor(Math.random()*934)
+      x=Math.floor(Math.random()*958)
       hp1=(hp[x]);
   atk1=(atk[x]);
   df1=(df[x]);
@@ -97,6 +97,32 @@ function newgame(){
   spd1=(spd[x]);
   console.log(hp[x]+atk[x]+df[x]+spa[x]+sdf[x]+spd[x]);
   choosegen();
+    }
+  }
+  if (document.getElementById("goodness").checked){
+    while ((hp[x]+atk[x]+df[x]+spa[x]+sdf[x]+spd[x])<450){
+      x=Math.floor(Math.random()*958)
+  hp1=(hp[x]);
+  atk1=(atk[x]);
+  df1=(df[x]);
+  spa1=(spa[x]);
+  sdf1=(sdf[x]);
+  spd1=(spd[x]);
+  console.log(hp[x]+atk[x]+df[x]+spa[x]+sdf[x]+spd[x]);
+  choosegen();
+  if (document.getElementById("Generations").value !="all"){
+    while (gen!=parseInt(document.getElementById("Generations").value)){
+      x=Math.floor(Math.random()*958)
+      hp1=(hp[x]);
+  atk1=(atk[x]);
+  df1=(df[x]);
+  spa1=(spa[x]);
+  sdf1=(sdf[x]);
+  spd1=(spd[x]);
+  console.log(hp[x]+atk[x]+df[x]+spa[x]+sdf[x]+spd[x]);
+  choosegen();
+    }
+  }
     }
   }
   console.log('Gen'+ ' '+(gen));
