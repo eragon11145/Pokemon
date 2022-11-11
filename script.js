a7aeab0e8a4a0fb8573bbb5b56403a72041f1ae6
eragon11145 <109441971+eragon11145@users.noreply.github.com>
2022-11-10 20:57:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ x=(Math.random())
 console.log(x)
 y=Math.random()
 guesses=0
-x=Math.floor(x * 958);
-y=Math.floor(y * 958);
+x=Math.floor(x * 1071);
+y=Math.floor(y * 1071);
 pokemon=" Bulbasaur, Ivysaur, Venusaur, Mega venusaur, Charmander, Charmeleon, Charizard, Mega charizard x, Mega charizard y, Squirtle, Wartortle, Blastoise, Mega blastoise, Caterpie, Metapod, Butterfree, Weedle, Kakuna, Beedrill, Mega beedrill, Pidgey, Pidgeotto, Pidgeot, Mega Pidgeot, Rattata, Raticate, Spearow, Fearow, Ekans, Arbok, Pikachu, Raichu, Sandshrew, Sandslash, Nidoran, Nidorina, Nidoqueen, Nidoran, Nidorino, Nidoking, Clefairy, Clefable, Vulpix, Ninetales, Jigglypuff, Wigglytuff, Zubat, Golbat, Oddish, Gloom, Vileplume, Paras, Parasect, Venonat, Venomoth, Diglett, Dugtrio, Meowth, Persian, Psyduck, Golduck, Mankey, Primeape, Growlithe, Arcanine, Poliwag, Poliwhirl, Poliwrath, Abra, Kadabra, Alakazam, Mega alakazam, Machop, Machoke, Machamp, Bellsprout, Weepinbell, Victreebel, Tentacool, Tentacruel, Geodude, Graveler, Golem, Ponyta, Rapidash, Slowpoke, Slowbro, Mega slowbro, Magnemite, Magneton, Farfetch’d, Doduo, Dodrio, Seel, Dewgong, Grimer, Muk, Shellder, Cloyster, Gastly, Haunter, Gengar, Mega gengar, Onix, Drowzee, Hypno, Krabby, Kingler, Voltorb, Electrode, Exeggcute, Exeggutor, Cubone, Marowak, Hitmonlee, Hitmonchan, Lickitung, Koffing, Weezing, Rhyhorn, Rhydon, Chansey, Tangela, Kangaskhan, Mega kangaskhan, Horsea, Seadra, Goldeen, Seaking, Staryu, Starmie, Mr. Mime, Scyther, Jynx, Electabuzz, Magmar, Pinsir, Mega pinsir, Tauros, Magikarp, Gyarados, Mega gyarados, Lapras, Ditto, Eevee, Vaporeon, Jolteon, Flareon, Porygon, Omanyte, Omastar, Kabuto, Kabutops, Aerodactyl, Mega aerodactyl, Snorlax, Articuno, Zapdos, Moltres, Dratini, Dragonair, Dragonite, Mewtwo, Mega mewtwo x, Mega mewtwo y, Mew, Chikorita, Bayleef, Meganium, Cyndaquil, Quilava, Typhlosion, Totodile, Croconaw, Feraligatr, Sentret, Furret, Hoothoot, Noctowl, Ledyba, Ledian, Spinarak, Ariados, Crobat, Chinchou, Lanturn, Pichu, Cleffa, Igglybuff, Togepi, Togetic, Natu, Xatu, Mareep, Flaaffy, Ampharos, Bellossom, Marill, Azumarill, Sudowoodo, Politoed, Hoppip, Skiploom, Jumpluff, Aipom, Sunkern, Sunflora, Yanma, Wooper, Quagsire, Espeon, Umbreon, Murkrow, Slowking, Misdreavus, Unown, Wobbuffet, Girafarig, Pineco, Forretress, Dunsparce, Gligar, Steelix, Snubbull, Granbull, Qwilfish, Scizor, Shuckle, Heracross, Sneasel, Teddiursa, Ursaring, Slugma, Magcargo, Swinub, Piloswine, Corsola, Remoraid, Octillery, Delibird, Mantine, Skarmory, Houndour, Houndoom, Kingdra, Phanpy, Donphan, Porygon2, Stantler, Smeargle, Tyrogue, Hitmontop, Smoochum, Elekid, Magby, Miltank, Blissey, Raikou, Entei, Suicune, Larvitar, Pupitar, Tyranitar, Lugia, Ho-oh, Celebi, Treecko, Grovyle, Sceptile, Torchic, Combusken, Blaziken, Mudkip, Marshtomp, Swampert, Poochyena, Mightyena, Zigzagoon, Linoone, Wurmple, Silcoon, Beautifly, Cascoon, Dustox, Lotad, Lombre, Ludicolo, Seedot, Nuzleaf, Shiftry, Taillow, Swellow, Wingull, Pelipper, Ralts, Kirlia, Gardevoir, Surskit, Masquerain, Shroomish, Breloom, Slakoth, Vigoroth, Slaking, Nincada, Ninjask, Shedinja, Whismur, Loudred, Exploud, Makuhita, Hariyama, Azurill, Nosepass, Skitty, Delcatty, Sableye, Mawile, Aron, Lairon, Aggron, Meditite, Medicham, Electrike, Manectric, Plusle, Minun, Volbeat, Illumise, Roselia, Gulpin, Swalot, Carvanha, Sharpedo, Wailmer, Wailord, Numel, Camerupt, Torkoal, Spoink, Grumpig, Spinda, Trapinch, Vibrava, Flygon, Cacnea, Cacturne, Swablu, Altaria, Zangoose, Seviper, Lunatone, Solrock, Barboach, Whiscash, Corphish, Crawdaunt, Baltoy, Claydol, Lileep, Cradily, Anorith, Armaldo, Feebas, Milotic, Castform, Kecleon, Shuppet, Banette, Duskull, Dusclops, Tropius, Chimecho, Absol, Wynaut, Snorunt, Glalie, Spheal, Sealeo, Walrein, Clamperl, Huntail, Gorebyss, Relicanth, Luvdisc, Bagon, Shelgon, Salamence, Beldum, Metang, Metagross, Regirock, Regice, Registeel, Latias, Latios, Kyogre, Groudon, Rayquaza, Jirachi, Deoxys, Turtwig, Grotle, Torterra, Chimchar, Monferno, Infernape, Piplup, Prinplup, Empoleon, Starly, Staravia, Staraptor, Bidoof, Bibarel, Kricketot, Kricketune, Shinx, Luxio, Luxray, Budew, Roserade, Cranidos, Rampardos, Shieldon, Bastiodon, Burmy, Wormadam, Mothim, Combee, Vespiquen, Pachirisu, Buizel, Floatzel, Cherubi, Cherrim, Shellos, Gastrodon, Ambipom, Drifloon, Drifblim, Buneary, Lopunny, Mismagius, Honchkrow, Glameow, Purugly, Chingling, Stunky, Skuntank, Bronzor, Bronzong, Bonsly, Mime Jr., Happiny, Chatot, Spiritomb, Gible, Gabite, Garchomp, Munchlax, Riolu, Lucario, Hippopotas, Hippowdon, Skorupi, Drapion, Croagunk, Toxicroak, Carnivine, Finneon, Lumineon, Mantyke, Snover, Abomasnow, Weavile, Magnezone, Lickilicky, Rhyperior, Tangrowth, Electivire, Magmortar, Togekiss, Yanmega, Leafeon, Glaceon, Gliscor, Mamoswine, Porygon-Z, Gallade, Probopass, Dusknoir, Froslass, Rotom, Uxie, Mesprit, Azelf, Dialga, Palkia, Heatran, Regigigas, Giratina, Cresselia, Phione, Manaphy, Darkrai, Shaymin, Arceus, Victini, Snivy, Servine, Serperior, Tepig, Pignite, Emboar, Oshawott, Dewott, Samurott, Patrat, Watchog, Lillipup, Herdier, Stoutland, Purrloin, Liepard, Pansage, Simisage, Pansear, Simisear, Panpour, Simipour, Munna, Musharna, Pidove, Tranquill, Unfezant, Blitzle, Zebstrika, Roggenrola, Boldore, Gigalith, Woobat, Swoobat, Drilbur, Excadrill, Audino, Timburr, Gurdurr, Conkeldurr, Tympole, Palpitoad, Seismitoad, Throh, Sawk, Sewaddle, Swadloon, Leavanny, Venipede, Whirlipede, Scolipede, Cottonee, Whimsicott, Petilil, Lilligant, Basculin, Sandile, Krokorok, Krookodile, Darumaka, Darmanitan, Maractus, Dwebble, Crustle, Scraggy, Scrafty, Sigilyph, Yamask, Cofagrigus, Tirtouga, Carracosta, Archen, Archeops, Trubbish, Garbodor, Zorua, Zoroark, Minccino, Cinccino, Gothita, Gothorita, Gothitelle, Solosis, Duosion, Reuniclus, Ducklett, Swanna, Vanillite, Vanillish, Vanilluxe, Deerling, Sawsbuck, Emolga, Karrablast, Escavalier, Foongus, Amoonguss, Frillish, Jellicent, Alomomola, Joltik, Galvantula, Ferroseed, Ferrothorn, Klink, Klang, Klinklang, Tynamo, Eelektrik, Eelektross, Elgyem, Beheeyem, Litwick, Lampent, Chandelure, Axew, Fraxure, Haxorus, Cubchoo, Beartic, Cryogonal, Shelmet, Accelgor, Stunfisk, Mienfoo, Mienshao, Druddigon, Golett, Golurk, Pawniard, Bisharp, Bouffalant, Rufflet, Braviary, Vullaby, Mandibuzz, Heatmor, Durant, Deino, Zweilous, Hydreigon, Larvesta, Volcarona, Cobalion, Terrakion, Virizion, Tornadus, Thundurus, Reshiram, Zekrom, Landorus, Kyurem, Keldeo, Meloetta, Genesect, Chespin, Quilladin, Chesnaught, Fennekin, Braixen, Delphox, Froakie, Frogadier, Greninja, Bunnelby, Diggersby, Fletchling, Fletchinder, Talonflame, Scatterbug, Spewpa, Vivillon, Litleo, Pyroar, Flabebe, Floette, Florges, Skiddo, Gogoat, Pancham, Pangoro, Furfrou, Espurr, Meowstic, Honedge, Doublade, Aegislash, Spritzee, Aromatisse, Swirlix, Slurpuff, Inkay, Malamar, Binacle, Barbaracle, Skrelp, Dragalge, Clauncher, Clawitzer, Helioptile, Heliolisk, Tyrunt, Tyrantrum, Amaura, Aurorus, Sylveon, Hawlucha, Dedenne, Carbink, Goomy, Sliggoo, Goodra, Klefki, Phantump, Trevenant, Pumpkaboo, Gourgeist, Bergmite, Avalugg, Noibat, Noivern, Xerneas, Yveltal, Zygarde, Diancie, Hoopa, Volcanion, Rowlet, Dartrix, Decidueye, Litten, Torracat, Incineroar, Popplio, Brionne, Primarina, Pikipek, Trumbeak, Toucannon, Yungoos, Gumshoos, Grubbin, Charjabug, Vikavolt, Crabrawler, Crabominable, Oricorio, Cutiefly, Ribombee, Rockruff, Lycanroc, Wishiwashi, Mareanie, Toxapex, Mudbray, Mudsdale, Dewpider, Araquanid, Fomantis, Lurantis, Morelull, Shiinotic, Salandit, Salazzle, Stufful, Bewear, Bounsweet, Steenee, Tsareena, Comfey, Oranguru, Passimian, Wimpod, Golisopod, Sandygast, Palossand, Pyukumuku, Type: Null, Silvally, Minior, Komala, Turtonator, Togedemaru, Mimikyu, Bruxish, Drampa, Dhelmise, Jangmo-o, Hakamo-o, Kommo-o, Alolan rattata, Alolan raticate, Alolan raichu, Alolan sandshrew, Alolan sandslash, Alolan vulpix, Alolan ninetales, Alolan diglett, Alolan dugtrio, Alolan meowth, Alolan persian, Alolan geodude, Alolan graveler, Alolan golem, Alolan grimer, Alolan muk, Alolan exeggutor, Alolan marowak, Tapu koko, Tapu lele, Tapu bulu, Tapu fini, Cosmog, Cosmoem, Solgaleo, Lunala, Nihilego, Buzzwole, Pheromosa, Xurkitree, Celesteela, Kartana, Guzzlord, Necrozma, Magearna, Marshadow, Poipole, Naganadel, Stakataka, Blacephalon, Zeraora, Meltan, Melmetal, Grookey, Thwackey, Rillaboom, Scorbunny, Raboot, Cinderace, Sobble, Drizzile, Inteleon, Skwovet, Greedent, Rookidee, Corvisquire, Corviknight, Blipbug, Dottler, Orbeetle, Nickit, Thievul, Gossifleur, Eldegoss, Wooloo, Dubwool, Chewtle, Drednaw, Yamper, Boltund, Rolycoly, Carkol, Coalossal, Applin, Flapple, Appletun, Silicobra, Sandaconda, Cramorant, Arrokuda, Barraskewda, Toxel, Toxtricity, Sizzlipede, Centiskorch, Clobbopus, Grapploct, Sinistea, Polteageist, Hatenna, Hattrem, Hatterene, Impidimp, Morgrem, Grimmsnarl, Galarian zigzagoon, Galarian linoone, Obstagoon, Galarian meowth, Perrserker, Galarian ponyta, Galarian rapidash, Galarian slowpoke, Galarian slowbro, Galarian slowking, Galarian corsola, Cursola, Galarian farfetch’d, Sirfetchd, Galarian weezing, Galarian mr. mime, Mr. rime, Galarian darumaka, Galarian darmanitan, Galarian yamask, Runerigus, Galarian stunfisk, Milcery, Alcremie, Falinks, Pincurchin, Snom, Frosmoth, Stonjourner, Eiscue, Indeedee, Morpeko, Cufant, Copperajah, Dracozolt, Arctozolt, Dracovish, Arctovish, Duraludon, Dreepy, Drakloak, Dragapult, Zacian, Zamazenta, Eternatus, Kubfu, Urshifu, Zarude, Regieleki, Regidrago, Glastrier, Spectrier, Calyrex, Galarian articuno, Galarian zapdos, Galarian moltres, Wyrdeer, Kleavor, Ursaluna, Basculegion, Basculegion, Sneasler, Overqwil, Enamorus, Enamorus, Sprigatito, Floragato, Meowscarada, Fuecoco, Crocolar, Skeledirge, Quaxley, Quaxwell, Quaquaval, Lechonk, Oinkologne, Oinkologne, Dudunsparce, Dudunsparce, Tarountula, Spidops, Nymble, Lokix, Rellor, Rabsca, Greavard, Houndstone, Flittle, Espathra, Farigarif, Wiglett, Wugtrio, Dondozo, Veluza, Finizen, Palafin, Palafin, Smoliv, Dolliv, Arboliva, Capsakid, Scovillian, Tadbulb, Bellibolt, Varoom, Revavroom, Orthworm, Tandemaus, Maushold, Maushold, Cetoddle, Cetitan, Frigibax, Arctibax, Baxcalibur, Tatsugiri, Tatsugiri, Tatsugiri, Cyclizar, Pawmi, Pawmo, Pawmot, Wattrel, Kilowattrel, Bombirdier, Squawkabilly, Squawkabilly, Squawkabilly, Squawkabilly, Flamigo, Klawf, Nacli, Naclstack, Garganacl, Glimmet, Glimmora, Shroodle, Grafaiai, Fidough, Dachsbun, Maschiff, Mabosstiff, Bramblin, Brambleghast, Gimmighoul, Gimmighoul, Gholdengo, Great tusk, Brute bonnet, Sandy shocks, Scream tail, Flutter mane, Slither wing, Roaring moon, Iron treads, Iron moth, Iron hands, Iron jugulis, Iron thorns, Iron bundle, Iron valiant, Ting-lu, Chien-pao, Wo-chien, Chi-yu, Koraidon, Miraidon, Tinkatink, Tinkatuff, Tinkaton, Charcadet, Armarouge, Ceruledge, Toedscool, Toedscruel, Kingambit, Clodsire, Annihilape, null";
 pokemoN=pokemon.split(",");
 pokemoN2=pokemon.split(",");
@@ -38,7 +38,7 @@ function stats(poke){
   console.log(hp[i]+atk[i]+df[i]+spa[i]+sdf[i]+spd[i]);
   }
 }
-x=Math.floor(Math.random()*958);
+x=Math.floor(Math.random()*1071);
 console.log(hp[x]);
 console.log(atk[x]);
 console.log(df[x]);
@@ -68,7 +68,10 @@ function choosegen(){
   }
   else if (x<842){
     gen=7;
-  }
+  }    
+  if ((841<x) && (x<958)){
+      gen=8;
+    }
   else{
     gen=8;
   }
@@ -103,7 +106,7 @@ document.getElementById("SDFBar").style.visibility="visible"
   choosegen();
   if (document.getElementById("Generations").value !="all"){
     while (gen!=parseInt(document.getElementById("Generations").value)){
-      x=Math.floor(Math.random()*958)
+      x=Math.floor(Math.random()*1071)
       hp1=(hp[x]);
   atk1=(atk[x]);
   df1=(df[x]);
@@ -116,7 +119,7 @@ document.getElementById("SDFBar").style.visibility="visible"
   }
   if (document.getElementById("goodness").checked){
     while ((hp[x]+atk[x]+df[x]+spa[x]+sdf[x]+spd[x])<450){
-      x=Math.floor(Math.random()*958)
+      x=Math.floor(Math.random()*1071)
   hp1=(hp[x]);
   atk1=(atk[x]);
   df1=(df[x]);
@@ -127,7 +130,7 @@ document.getElementById("SDFBar").style.visibility="visible"
   choosegen();
   if (document.getElementById("Generations").value !="all"){
     while (gen!=parseInt(document.getElementById("Generations").value)){
-      x=Math.floor(Math.random()*958)
+      x=Math.floor(Math.random()*1071)
       hp1=(hp[x]);
   atk1=(atk[x]);
   df1=(df[x]);
@@ -201,10 +204,13 @@ function guess(poke){
     if ((735<i) && (i<842)){
       gen2=7;
     }
-    if (841<i){
+    if ((841<i) && (i<958)){
       gen2=8;
     }
-    if (i===960){
+    if (957<i){
+      gen2=9;
+    }
+    if (i===1073){
       gen2='Null';
     }
     choosegen()
